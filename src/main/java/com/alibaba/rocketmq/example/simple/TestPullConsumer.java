581ca0eb1ba882fa83f7531bf8825b6b6a4baf26
zheng <drcross@outlook.com>
2016-09-29 16:04:18 +0800

    do some re-arrangement

--- src/main/java/com/alibaba/rocketmq/example/simple/TestPullConsumer.java
+++ src/main/java/com/alibaba/rocketmq/example/simple/TestPullConsumer.java
@@ -4,33 +4,34 @@
 
 package com.alibaba.rocketmq.example.simple;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-import java.util.Timer;
-import java.util.TimerTask;
-import java.util.concurrent.TimeUnit;
 import com.alibaba.rocketmq.client.consumer.DefaultMQPullConsumer;
 import com.alibaba.rocketmq.client.consumer.PullResult;
 import com.alibaba.rocketmq.client.exception.MQClientException;
-import com.alibaba.rocketmq.common.SystemClock;
 import com.alibaba.rocketmq.common.message.MessageExt;
 import com.alibaba.rocketmq.common.message.MessageQueue;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 
 public class TestPullConsumer {
     private static Map<MessageQueue, Long> offseTable = new HashMap<MessageQueue, Long>();
 
 
-    public static void main(String[] args) throws MQClientException {
+    public static void main(String[] args) throws MQClientException, InterruptedException {
         DefaultMQPullConsumer consumer = new DefaultMQPullConsumer("please_rename_unique_group_name_5");
         String topic = "TopicTest1";
 
-//        consumer.setVipChannelEnabled(false);
-//        consumer.setNamesrvAddr("192.168.1.5:9876");
-        consumer.setNamesrvAddr("192.168.31.58:9876");
+        // for rocketmq server v3.2.6
+        consumer.setVipChannelEnabled(false);
+        consumer.setNamesrvAddr("192.168.1.5:9876");
+        // for rocketmq server v3.5.8
+//        consumer.setNamesrvAddr("192.168.31.58:9876");
 
+
+        consumer.setPersistConsumerOffsetInterval(1000);
         System.out.println("default persiste interval: " + consumer.getPersistConsumerOffsetInterval());
-//        consumer.setPersistConsumerOffsetInterval(1000);
 
 
 
@@ -46,6 +47,7 @@ public class TestPullConsumer {
 //        consumer.registerMessageQueueListener(topic, null);
         //循环每一个队列
         for (MessageQueue mq : mqs) {
+
             System.out.println("######################## a new queue ########################");
             System.out.println("Consume message from queue: " + mq + " mqsize=" + mqs.size());
 
@@ -53,19 +55,14 @@ public class TestPullConsumer {
             boolean isEmptyQueue = true;
             int counter = 0;
             //每个队列里无限循环，分批拉取未消费的消息，直到拉取不到新消息为止
-
-
             SINGLE_MQ:
             while (counter++ < 100) {
                 try {
-
                     long offset = consumer.fetchConsumeOffset(mq, false);
                     offset = offset < 0 ? 0 : offset;
                     System.out.println("消费进度 Offset: " + offset);
                     PullResult result = consumer.pull(mq, null, offset, 10);
                     System.out.println("接收到的消息集合" + result);
-
-
 
 
                     switch (result.getPullStatus()) {
@@ -80,7 +77,6 @@ public class TestPullConsumer {
                                         System.out.println("pullResult.getMsgFoundList()消息体内容===="+ new String(me.getBody()) + " ==== " + me.getQueueOffset());
 
                                     }
-
                                 }
                             }
                             // 获取下一个下标位置
@@ -98,29 +94,8 @@ public class TestPullConsumer {
                             //拉取不到新消息，跳出 SINGLE_MQ 当前队列循环，开始下一队列循环。
 
 
-
-                            long newoffset;
-                            if (isEmptyQueue) { } else {
-
-                                java.lang.Thread.sleep(30000);
-
-
-//                                while (true) {
-//                                    newoffset = consumer.fetchConsumeOffset(mq, true);
-//                                    System.out.println("new offset == " + newoffset + " should be " + offset);
-//
-//                                 if (newoffset != offset) {
-//                                     java.lang.Thread.sleep(1000);
-//                                 } else {
-//                                     break;
-//                                 }
-//                                }
-                            }
-
                             System.out.println("Offset from store ==== " + consumer.fetchConsumeOffset(mq, true));
-
                             break SINGLE_MQ;
-
 
                         case OFFSET_ILLEGAL:
                             System.out.println("下标错误");
@@ -133,22 +108,14 @@ public class TestPullConsumer {
                     e.printStackTrace();
                 }
             }
+
+            if (isEmptyQueue) { } else {
+                Thread.sleep(1000 * 5);
+            }
+
         }
 
         consumer.shutdown();
-//        // 定义一个定时器，用于测试pull方法时，模拟延时以便自动更新消费进度操作，生产环境中，因consumer一直在运行，因此不需要此步操作。
-//        final Timer timer = new Timer("TimerThread", true);
-//        // 定时器延时30秒后，关闭cousumer，因为客户端从首次启动时在1000*10ms即10秒后，后续每5秒定期执行一次（由参数：persistConsumerOffsetInterval控制）向本机及broker端回写记录消费进度，
-//        // 因此consumer启动后需要延时至少15秒才能执行回写操作，否则下次运行pull方法时，因上次未能及时更新消费进度，程序会重复取出上次消费过的消息重新消费，所以此处延时30秒，留出回写的时间
-//        timer.schedule(new TimerTask() {
-//            @Override
-//            public void run() {
-//                consumer.shutdown();
-//                // 如果只要这个延迟一次，用cancel方法取消掉．
-//                this.cancel();
-//            }
-//        }, 3);
-
     }
 
     private static void putMessageQueueOffset(MessageQueue mq, long offset) {
